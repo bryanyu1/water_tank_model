ad8bafe2c895134410803fb1ccea2c66e3772d4f
Bryan Yu <70780073+bryanyu1@users.noreply.github.com>
2021-04-05 11:45:03 -0700

    Update simulate.m

--- simulate.m
+++ simulate.m
@@ -2,7 +2,7 @@ function [y] = simulate(r1,l1,l2,r2,N,tol)
     V = @(t) 3 * t + 2 * t.^2 + cos(4 * t) * exp(-t/4) - 1;
     dV = @(t) 4 * t - (cos(4 * t) * exp(-t/4))/4 - 4 * sin(4 * t) * exp(-t/4) + 3
     
-    sph = (4 / 3) * pi * r1.^3;
+    sph = (4 / 3) * pi * r1.^3; 
     cone = (1 / 3) * pi * r2.^2 * l1;
     cyl = pi * r2.^2 * l2;
     hem = (2 / 3) * pi * r2.^3; 
