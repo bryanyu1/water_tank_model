423d25f8d079a4740d25bd9f8c6d1263139948b4
Bryan Yu <70780073+bryanyu1@users.noreply.github.com>
2021-04-22 07:06:19 -0700

    Update solver.m

--- solver.m
+++ solver.m
@@ -2,7 +2,7 @@ function [x] = solver(f,df,x0,tol)
     k = 2; 
     y(1) = x0 - (f(x0)/df(x0));
     y(2) = y(1) - (f(y(1)) / df(y(1)));
-    while abs(y(k) - y(k - 1)) > tol 
+    while abs(y(k) - y(k - 1)) > tol
         k = k + 1;
         y(k) = y(k - 1) - (f(y(k - 1)) / df(y(k - 1))); 
     end
